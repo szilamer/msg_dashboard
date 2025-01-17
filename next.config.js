f49e2c50a41f62f7459e23d52ba50f5b9f31a94a
Szialmer Feher <szilamer@gmail.com>
2025-01-17 18:31:48 +0100

    fix: Next.js API konfiguráció

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  api: {
+    bodyParser: true,
+    externalResolver: true,
+  },
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
     DIRECT_URL: process.env.DIRECT_URL,
