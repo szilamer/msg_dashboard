b08536fcb2572ba1c190605ec8c57c99b9b1a795
Szialmer Feher <szilamer@gmail.com>
2025-01-17 16:01:45 +0100

    feat: Add Next.js edge runtime config

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+  }
+}
+
+module.exports = nextConfig 
